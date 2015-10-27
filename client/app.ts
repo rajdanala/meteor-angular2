f34d9d61b6ac7de132012de8719c50f32e855d3a
Rajesh Danala <rajesh.danala@gmail.com>
2015-10-26 21:31:47 -0400

    dynamic template

--- client/app.ts
+++ client/app.ts
@@ -1,12 +1,27 @@
 /// <reference path="../typings/angular2-meteor.d.ts" />
-import {Component, View, bootstrap} from 'angular2/angular2';
+import {Component,NgFor, View, bootstrap} from 'angular2/angular2';
 
 @Component({
 	selector: 'app'
 })
 @View({
-	templateUrl: 'client/app.html'
+	templateUrl: 'client/app.html',
+	directives: [NgFor]
 })
-class MeteorAngular2 { }
+class MeteorAngular2 {
+	menus: Array<Object>;
+	constructor () {
+		this.menus = [
+			{
+			'name':'Home',
+			'page':'Welcome Home'
+			},
+			{
+			'name':'Contact Us',
+			'page':'Address'
+			}
+		];
+	}
+}
 
 bootstrap(MeteorAngular2);
